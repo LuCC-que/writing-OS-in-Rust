078c5c6d9fc085eb1da249837392dbc31d4d1559
LuCC-que <chen1968493689@gmail.com>
2022-08-31 18:22:37 -0400

    VGA display supported

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-// src/main.rs
-
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -11,18 +10,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    //VGA buffer is in 0xb8000
+    vga_buffer::print_something();
     loop {}
 }
